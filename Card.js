a5f78fa247dcc4ecbebcab975eb7849c30c96c28
uzair <uzair.shayansolutions@gmail.com>
2023-02-07 13:24:32 +0500

    fix detail card search tab bug

--- Card.js
+++ Card.js
@@ -1,6 +1,4 @@
 const CardContainer = document.getElementById("CardsContainer");
-const inputSearch = document.getElementById("myInput");
-const filter = document.getElementById("filter");
 const backButton = document.getElementById("backButton");
 const toggleMode = document.getElementById("toggle-mode");
 const Body = document.querySelector("body");
@@ -108,39 +106,6 @@ toggleMode.addEventListener("change", () => {
     );
     switchMode = true;
   }
-});
-// Search by country name
-
-inputSearch.addEventListener("keyup", (e) => {
-  if (e.key === "Enter") {
-    window.location.assign(
-      "file:///C:/Users/HP/Desktop/Rest%20Api/SearchByCountry.html"
-    );
-    sessionStorage.setItem("value", e.target.value);
-  }
-});
-
-// Filter Functionality
-
-const filterRegion = async () => {
-  let Url = "https://restcountries.com/v3.1/all";
-
-  let response = await fetch(Url);
-  let data = await response.json();
-  data.map((element) => {
-    const option = document.createElement("option");
-    option.innerText = element.region;
-    filter.appendChild(option);
-  });
-};
-
-filter.addEventListener("change", (event) => {
-  if (event.target.value === "") return;
-
-  CardContainer.innerHTML = "";
-  const Url = `https://restcountries.com/v3.1/region/${event.target.value}`;
-  backButton.style.display = "block";
-  RenderFetchData(Url);
 });
 
 // window.addEventListener("load", filterRegion);
